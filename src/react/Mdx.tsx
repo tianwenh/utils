66b564573fdc7640428762be67d361e30fdb6c6f
tianwenh <huastronmy@gmail.com>
2022-02-11 13:45:16 -0800

    fix mdx link type

--- src/react/Mdx.tsx
+++ src/react/Mdx.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { AnchorHTMLAttributes } from 'react';
 import { Link } from 'react-router-dom';
 
-export const MdxLink: React.FC<{ href: string; title: string }> = (props) => {
-  const isExternal = /^https?:\/\//.test(props.href);
+export const MdxLink: React.FC<AnchorHTMLAttributes<HTMLAnchorElement>> = (
+  props
+) => {
+  const href = props.href ?? '';
+  const isExternal = /^https?:\/\//.test(href);
   if (isExternal) {
     return <a target="_blank" {...props} />;
   }
   // Drop wiki link extension.
-  const dropExtension = props.href.replace(/\.mdx?/, '');
+  const dropExtension = href.replace(/\.mdx?/, '');
   // <Link /> breaks header anchor.
   if (dropExtension.startsWith('#')) {
     return <a {...props} />;
