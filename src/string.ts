c63b9595f0d3488df5af9366508ca57646f11521
tianwenh <huastronmy@gmail.com>
2022-02-12 15:14:10 -0800

    replace slug with github slug

--- src/string.ts
+++ src/string.ts
@@ -1,11 +1,6 @@
-export function slugify(text: string): string {
-  return text
-    .toLowerCase()
-    .trim()
-    .replace(/[^\w\s-]/g, '-')
-    .replace(/[\s_-]+/g, '-')
-    .replace(/^-+|-+$/g, '');
-}
+import BananaSlug from 'github-slugger';
+
+export const slugify = BananaSlug.slug;
 
 export function className(names: Record<string, boolean>): string {
   const activeNames = Object.entries(names)
